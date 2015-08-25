555d969aa6eb5ca3d78b457222b1b839217f7675
Arun Prakash <arun@entrayn.com>
2015-08-25 10:58:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -47,37 +47,6 @@ function focus(originalLeft) {
     }, 800);
 }
 
-// $(".wrapper").on("click", ".arrow", function() {
-//     var stripPos = $strip.position();
-//     if (this.id == "lft") {
-//         $strip.css({"left": stripPos.left + (wrapWidth / 2)});
-//     } else {
-//         $strip.css({"left": stripPos.left - (wrapWidth / 2)});
-//     }
-// });
-
-
-// $("#lft-arrow").click(function () {
-//   var leftPos = $('#numWrap').scrollLeft();
-// $("#numWrap").stop().animate({scrollLeft: leftPos - 200}, 800);
-// });
-
-// $("#rgt-arrow").click(function () {
-//   var leftPos = $('#numWrap').scrollLeft();
-// $("#numWrap").stop().animate({scrollLeft: leftPos + 200}, 800);
-// });
-
-// function leftArrow(){
-//     console.log("in left")
-//     var leftPos = $('#numWrap').scrollLeft();
-// $("#numWrap").stop().animate({scrollLeft: leftPos - 200}, 800);
-// }
-
-// function rightArrow(){
-//     var leftPos = $('#numWrap').scrollLeft();
-// $("#numWrap").stop().animate({scrollLeft: leftPos + 200}, 800);
-// }
-
 
 var leftArrow=function(){
     console.log("in left")
@@ -92,23 +61,5 @@ $("#numWrap").animate({scrollLeft: leftPos + 200}, 800);
 
 
 
-
-// var throttledLeft = _.throttle(leftArrow, 100);
-// var throttledRight=_.throttle(rightArrow,100);
-
-
-
-// $("#lft-arrow").on('click',function () {
-//     console.log("in leftttt")
-// $("#numWrap").on('scroll',throttledLeft);
-// });
-
-// $("#rgt-arrow").on('click',function () {
-// $("#numWrap").on('scroll',throttledRight);
-// });
-
-//$(window).on('scroll', throttledHandler);
-// $("TEXTINPUT").keyup(_.throttle(function () {...}, 150));
-
-$("#lft-arrow").click(_.throttle(leftArrow,1000));
-$("#rgt-arrow").click(_.throttle(rightArrow,1000));
+$("#lft-arrow").click(_.throttle(leftArrow,1200));
+$("#rgt-arrow").click(_.throttle(rightArrow,1200));
